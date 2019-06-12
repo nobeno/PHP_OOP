48c96c358e71a2bd9ec087acec10d3c8cbee2970
daichi nb <nd940501@gmail.com>
2019-06-12 12:11:54 +0800

    Todoクラス作成、一覧表示のための関数追加
    一覧表示機能追加のため。

--- Models/Todo.php
+++ Models/Todo.php
@@ -2,7 +2,24 @@
 
 require_once('config/dbconnect.php');
 
-class Todo{
+class Todo
+{
+    private $table = 'tasks';
+    private $db_manager;
 
+    public function __construct()
+    {
+        $this->db_manager = new DbManager();
+        $this->db_manager->connect();
+    }
+
+    public function all()
+    {
+        $stmt = $this->db_manager->dbh->prepare('SELECT * FROM ' . $this->table . ' ORDER BY id DESC');
+        $stmt->execute();
+        $tasks = $stmt->fetchAll();
+
+        return $tasks;
+    }
     
 }
